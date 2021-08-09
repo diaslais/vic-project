341be9439efa9bf39515867d6c3e5e419c13f858
Lais Dias <laisdias099@gmail.com>
2021-08-09 16:01:58 -0300

    modifies data classes

--- app/src/main/java/com/laisd/moviesapp/data/model/MovieDetailResponse.kt
+++ app/src/main/java/com/laisd/moviesapp/data/model/MovieDetailResponse.kt
@@ -3,18 +3,24 @@ package com.laisd.moviesapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class MovieDetailResponse (
+    @SerializedName("id")
     val id: Int,
     @SerializedName("backdrop_path")
     val backdropPoster: String?,
+    @SerializedName("title")
     val title: String,
     @SerializedName("vote_average")
     val userRating: Float,
     @SerializedName("release_date")
     val releaseDate: String,
+    @SerializedName("releases")
     val releases: ReleasesListResponse,
+    @SerializedName("runtime")
     val runtime: Int?,
+    @SerializedName("genres")
     val genres: List<GenreResponse>,
     @SerializedName("overview")
     val synopsis: String?,
+    @SerializedName("credits")
     val credits: CreditsResponse
 )
--- app/src/main/java/com/laisd/moviesapp/data/model/MovieResponse.kt
+++ app/src/main/java/com/laisd/moviesapp/data/model/MovieResponse.kt
@@ -3,9 +3,11 @@ package com.laisd.moviesapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(
+        @SerializedName("id")
         val id: Int,
         @SerializedName("poster_path")
         val poster: String?,
+        @SerializedName("title")
         val title: String,
         @SerializedName("vote_average")
         val userRating: Float,
