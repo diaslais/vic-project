f21ea1328ecac88e3c9691369bc3a181180d68ce
Lais Dias <laisdias099@gmail.com>
2021-08-10 23:44:35 -0300

    set right backgrounds to filter by genre

--- app/src/main/java/com/laisd/moviesapp/presentation/mainscreen/adapter/GenresFilterAdapter.kt
+++ app/src/main/java/com/laisd/moviesapp/presentation/mainscreen/adapter/GenresFilterAdapter.kt
@@ -1,23 +1,27 @@
 package com.laisd.moviesapp.presentation.mainscreen.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.laisd.moviesapp.R
 
 class GenresFilterAdapter(
+    val context: Context,
+    val regularLists: () -> Unit,
     val genreListener: (genre: String) -> Unit
 ) : RecyclerView.Adapter<GenresFilterAdapter.GenresFilterViewHolder>() {
     var genresList = listOf<String>()
+    private var selectedGenrePosition: Int? = null
 
     inner class GenresFilterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val btnGenre = itemView.findViewById<TextView>(R.id.btnMovieGenre)
 
-        fun bind(genre: String) {
-            btnGenre.text = genre
-        }
+        fun bind(genre: String) { btnGenre.text = genre }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenresFilterViewHolder =
@@ -29,9 +33,35 @@ class GenresFilterAdapter(
         holder.bind(genresList[position])
         val genre = genresList[position]
 
-        holder.btnGenre.setOnClickListener {
-            println("CLICOU NO GENERO $genre")
+        var isSelected: Boolean
+
+        if (selectedGenrePosition == position) {
+            holder.btnGenre.setBackgroundResource(R.drawable.genre_selected)
+            holder.btnGenre.setTextColor(getColor(context, R.color.light_gray))
             genreListener(genre)
+            isSelected = true
+        } else {
+            holder.btnGenre.setBackgroundResource(R.drawable.genre_background_gray)
+            holder.btnGenre.setTextColor(getColor(context, R.color.gray3))
+            isSelected = false
+        }
+
+        holder.btnGenre.setOnClickListener {
+            if (isSelected) {
+                it.setBackgroundResource(R.drawable.genre_background_gray)
+                (it as Button).setTextColor(getColor(context, R.color.gray3))
+                regularLists()
+                isSelected = false
+            } else {
+                it.setBackgroundResource(R.drawable.genre_selected)
+                (it as Button).setTextColor(getColor(context, R.color.light_gray))
+                genreListener(genre)
+                isSelected = true
+            }
+            if (selectedGenrePosition != position) {
+                selectedGenrePosition?.let { notifyItemChanged(it) }
+                selectedGenrePosition = position
+            }
         }
     }
 }
