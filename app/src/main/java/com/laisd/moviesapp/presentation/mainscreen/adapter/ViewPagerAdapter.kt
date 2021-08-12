ea2e60dd92546d87568a2338a887e3d60b6c8257
Lais Dias <laisdias099@gmail.com>
2021-08-12 17:09:11 -0300

    add linear snap helper to movies recyclerview

--- app/src/main/java/com/laisd/moviesapp/presentation/mainscreen/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/laisd/moviesapp/presentation/mainscreen/adapter/ViewPagerAdapter.kt
@@ -1,17 +1,14 @@
 package com.laisd.moviesapp.presentation.mainscreen.adapter
 
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
 import com.laisd.moviesapp.R
 import com.laisd.moviesapp.domain.model.Movie
 
 class ViewPagerAdapter(
-    val viewpager: ViewPager2,
     private val isFavorite: (movieId: Int) -> Boolean,
     val favoriteListener: (movie: Movie) -> Unit,
     val itemClickListener: (movieId: Int) -> Unit
@@ -20,42 +17,16 @@ class ViewPagerAdapter(
     var dataSet: MutableList<List<Movie>> = mutableListOf(listOf(), listOf())
 
     inner class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         val recyclerView = itemView.findViewById<RecyclerView>(R.id.rvMovies)
 
         fun bind(list: List<Movie>) {
             val recyclerAdapter = MoviesRecyclerViewAdapter(isFavorite, favoriteListener, itemClickListener)
             recyclerView.adapter = recyclerAdapter
+            val snapHelper = LinearSnapHelper()
+            recyclerView.onFlingListener = null
+            snapHelper.attachToRecyclerView(recyclerView)
             recyclerAdapter.movieList = list
-        }
-
-        fun handleNestedScroll() {
-            recyclerView.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
-                var initialTouchPosition = 0
-                override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-                    when (e.action) {
-                        MotionEvent.ACTION_DOWN -> initialTouchPosition = e.x.toInt()
-                        MotionEvent.ACTION_MOVE -> {
-                            val isScrollingRight = e.x < initialTouchPosition
-                            val isScrollingLeft = e.x > initialTouchPosition
-
-                            val isRecyclerViewsLastItem =
-                                (recyclerView.layoutManager as LinearLayoutManager).findLastCompletelyVisibleItemPosition() == recyclerView.adapter!!.itemCount - 1
-                            val isRecyclerViewsFirstItem =
-                                (recyclerView.layoutManager as LinearLayoutManager).findFirstCompletelyVisibleItemPosition() == 0
-
-                            viewpager.isUserInputEnabled =
-                                (isScrollingRight && isRecyclerViewsLastItem) || (isScrollingLeft && isRecyclerViewsFirstItem)
-                        }
-                        MotionEvent.ACTION_UP -> {
-                            initialTouchPosition = 0
-                            viewpager.isUserInputEnabled = true
-                        }
-                    }
-                    return false
-                }
-                override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
-                override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
-            })
         }
     }
 
@@ -66,6 +37,5 @@ class ViewPagerAdapter(
 
     override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
         holder.bind(dataSet[position])
-        holder.handleNestedScroll()
     }
 }
