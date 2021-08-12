831bcdb9a2924095b11453d128329d8275800861
Lais Dias <laisdias099@gmail.com>
2021-08-12 16:59:30 -0300

    change constructors

--- app/src/main/java/com/laisd/moviesapp/presentation/mainscreen/NestedScrollableHost.kt
+++ app/src/main/java/com/laisd/moviesapp/presentation/mainscreen/NestedScrollableHost.kt
@@ -11,9 +11,7 @@ import androidx.viewpager2.widget.ViewPager2.ORIENTATION_HORIZONTAL
 import kotlin.math.absoluteValue
 import kotlin.math.sign
 
-class NestedScrollableHost : FrameLayout {
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+class NestedScrollableHost @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : FrameLayout(context, attrs) {
 
     private var touchSlop = 0
     private var initialX = 0f
